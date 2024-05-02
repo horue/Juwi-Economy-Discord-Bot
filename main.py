fcb4821518bd8e327739e81a186f38684046b24a
Jorge <jorgejapao00@gmail.com>
2024-05-02 18:46:00 -0300

    Base bot essentials added

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from discord.ext import commands
 import sqlite3
 from key import bot_key
 
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix = 'b!', case_insensitive = True, activity=discord.CustomActivity(name="Use 'b!help' for help"), status=discord.Status.online, intents=intents)
