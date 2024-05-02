8ea775d0596547226899058cacda15458a75aeba
Jorge <jorgejapao00@gmail.com>
2024-05-02 18:48:59 -0300

    Bot now goes online

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ from key import bot_key
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix = 'b!', case_insensitive = True, activity=discord.CustomActivity(name="Use 'b!help' for help"), status=discord.Status.online, intents=intents)
+
+
+@bot.event
+async def on_ready():
+  print('{0.user} is online.' . format(bot))
+
+
+bot.run(bot_key)
