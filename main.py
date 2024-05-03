db5119df010ffdfff9b1e1a8688217a86d598fee
Jorge <jorgejapao00@gmail.com>
2024-05-03 08:34:10 -0300

    Base help command added, requests imported

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import sqlite3
+import requests
 from key import bot_key
 
 
@@ -12,5 +13,16 @@ bot = commands.Bot(command_prefix = 'b!', case_insensitive = True, activity=disc
 @bot.event
 async def on_ready():
   print('{0.user} is online.' . format(bot))
+
+
+
+@bot.command()
+async def helpbank(ctx):
+  await ctx.send('Help for economy commands goes here.')
+
+@bot.command()
+async def bank(ctx):
+  await ctx.sendo('-')
+
 
 bot.run(bot_key)
